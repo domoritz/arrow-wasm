b222c8dc4aada10306e5e072e4927246219cea36
Dominik Moritz <domoritz@gmail.com>
2021-01-22 23:57:49 +0100

    clean up

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,6 @@
-use arrow::array::UInt32Array;
 use arrow::ipc;
-use wasm_bindgen::prelude::*;
-extern crate console_error_panic_hook;
-use std::panic;
 use std::io::Cursor;
+use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 extern "C" {
@@ -14,66 +11,12 @@ extern "C" {
     fn log_u32(a: u32);
 }
 
-#[wasm_bindgen(module = "buffer")]
-extern "C" {
-    type Buffer;
-
-    #[wasm_bindgen(method, js_name = toString)]
-    fn to_string(this: &Buffer) -> String;
-}
-
-#[wasm_bindgen(module = "fs")]
-extern "C" {
-    #[wasm_bindgen(js_name = readFileSync, catch)]
-    fn read_file(path: &str) -> Result<Buffer, JsValue>;
-}
-
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    log(&format!("Hello, {}!", name));
-}
-
-#[wasm_bindgen]
-pub fn length() -> usize {
-    let array = UInt32Array::from(vec![Some(1), None, Some(3)]);
-
-    return array.len();
-}
-
-#[wasm_bindgen]
-pub fn read(path: &str) -> usize {
-    log(&format!("Reading {}", path));
-
-    return 42;
-
-    // let buffer = read_file(path).unwrap();
-    // return parse(buffer);
-    // log(&buffer.to_string());
-    // let file = File::open(path).unwrap();
-
-    // let reader = ipc::reader::StreamReader::try_new(buffer);
-    // return reader.unwrap().num_batches();
-}
-
 #[wasm_bindgen]
 pub fn parse(contents: &[u8]) -> usize {
     let cursor = Cursor::new(contents);
-    let reader = ipc::reader::FileReader::try_new(cursor).unwrap();
-    return reader.num_batches();
+    let mut reader = ipc::reader::FileReader::try_new(cursor).unwrap();
+    
+    assert!(reader.num_batches() == 1);
+
+    return reader.next().unwrap().unwrap().num_rows();
 }
-
-// read("flights-10k.arrow")
-
-// #[wasm_bindgen]
-// pub fn load(url: &str) -> usize {
-//     log(&format!("Reading {}", path));
-
-//     let file = read_file(path).unwrap();
-//     // let file = File::open(path).unwrap();
-
-//     // let reader = ipc::reader::StreamReader::try_new(file);
-//     // return reader.unwrap().num_batches();
-
-//     return 42;
-// }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ extern crate arrow_wasm;
 use std::fs;
 
 fn main() {
-    println!("{}", arrow_wasm::length());
-
     let data = fs::read("flights-10k.arrow").expect("Could not read file");
 
     println!("{}", arrow_wasm::parse(&data));

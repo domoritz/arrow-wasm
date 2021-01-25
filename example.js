7df3956ff7cf189f26298e93e501321d7d65f4df
Dominik Moritz <domoritz@gmail.com>
2021-01-25 11:53:50 +0100

    implement all number vectors and boolean vector

--- example.js
+++ example.js
@@ -1,7 +1,5 @@
 const arrow_wasm = require("./pkg");
 const arrow_js = require("apache-arrow");
-const { ListBuilder } = require("apache-arrow");
-
 
 function rint(min, max) {
   let delta = min;
@@ -13,24 +11,27 @@ function rint(min, max) {
   return (min + delta * Math.random()) | 0;
 }
 
-function ints(n, min, max, nullf) {
+function ints(n, min, max, nullf, typed) {
   const data = [];
   for (let i = 0; i < n; ++i) {
     const v = nullf && Math.random() < nullf ? null : rint(min, max);
     data.push(v);
   }
+  if (typed) {
+    return new Int32Array(data);
+  }
   return data;
 }
 
 // const N = 1e7;
-const N = 1e3
+const N = 1e3;
 
-const values = ints(N, -1000, 1000);
+const values = ints(N, -1000, 1000, false);
 
 const vector_js = arrow_js.Int32Vector.from(values);
 const vector_wasm = arrow_wasm.Int32Vector.from(values);
 
-function run(a, wasm) {
+function run(a) {
   // console.log('Array', a)
 
   console.log("length", a.length);
@@ -46,15 +47,55 @@ function run(a, wasm) {
   console.log("sum", sum);
 
   console.log("toArray", a.toArray());
-  console.log("toJSON", a.toJSON());
+  if (N < 1e4) {
+    console.log("toJSON", a.toJSON());
+  }
+
+  console.log("toString", a.toString());
 }
 
-console.group("==== JS ====");
-run(vector_js, false);
+// try different types
+
+const vectors = []
+vectors.push(arrow_wasm.Int8Vector.from([1,2,3]));
+vectors.push(arrow_wasm.Int16Vector.from([1,2,3]));
+vectors.push(arrow_wasm.Int32Vector.from([1,2,3]));
+vectors.push(arrow_wasm.Int64Vector.from([1,2,3].map(BigInt)));
+
+vectors.push(arrow_wasm.UInt8Vector.from([1,2,3]));
+vectors.push(arrow_wasm.UInt16Vector.from([1,2,3]));
+vectors.push(arrow_wasm.UInt32Vector.from([1,2,3]));
+vectors.push(arrow_wasm.UInt64Vector.from([1,2,3].map(BigInt)));
+
+vectors.push(arrow_wasm.Float32Vector.from([1.1,2.2,3.3]));
+vectors.push(arrow_wasm.Float64Vector.from([1.1,2.2,3.3]));
+
+vectors.push(arrow_wasm.BooleanVector.from([5], 4));
+
+console.log(vectors.map((v) => v.toJSON()));
+
+// comparisons
+
+console.group("==== Native ====");
+
+console.time("scan");
+let sum = 0;
+for (let index = 0; index < N; index++) {
+  sum += values[index];
+}
+console.timeEnd("scan");
+console.log("sum", sum);
 console.groupEnd();
 
 console.log();
 
 console.group("==== WASM ====");
-run(vector_wasm, true);
+run(vector_wasm);
 console.groupEnd();
+
+console.log();
+
+console.group("==== JS ====");
+run(vector_js);
+console.groupEnd();
+

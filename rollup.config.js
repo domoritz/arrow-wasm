c17aa329e21fa74610c6e1acd9e1631e8ead860e
Dominik Moritz <domoritz@gmail.com>
2021-01-29 15:40:28 +0100

    try not to inline wasm

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,24 @@
 import typescript from "@rollup/plugin-typescript";
-import rust from "@wasm-tool/rollup-plugin-rust";
+import copy from "rollup-plugin-copy";
+import execute from "rollup-plugin-execute";
+import replace from "@rollup/plugin-replace";
 
 export default {
   input: "index.ts",
   output: {
     file: "dist/arrow.js",
     format: "umd",
-    sourcemap: true,
+    sourcemap: false,
     name: "arrow",
   },
   plugins: [
-    rust({
-      inlineWasm: true,
+    copy({
+      targets: [{ src: "pkg/arrow_wasm_bg.wasm", dest: "dist/arrow.wasm" }],
     }),
     typescript(),
+    replace({
+      "arrow_wasm.js": "arrow.js",
+      "_bg.wasm": ".wasm",
+    }),
   ],
 };

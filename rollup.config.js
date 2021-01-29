09fcc99b508931cdbf66a112708f7c6f872437c1
Dominik Moritz <domoritz@gmail.com>
2021-01-29 09:47:30 +0100

    chore: dev setup improvements for contirbutors

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,16 @@
-
 import rust from "@wasm-tool/rollup-plugin-rust";
 
 export default {
-    input: "index.js",
-    output: {
-        file: "dist/arrow.js",
-        format: "umd",
-        sourcemap: true,
-        name: "arrow"
-    },
-    plugins: [
-        rust({
-            inlineWasm: true
-        }),
-    ],
+  input: "index.js",
+  output: {
+    file: "dist/arrow.js",
+    format: "umd",
+    sourcemap: true,
+    name: "arrow",
+  },
+  plugins: [
+    rust({
+      inlineWasm: true,
+    }),
+  ],
 };

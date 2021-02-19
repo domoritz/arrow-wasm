30f4521d8f47f3536d52df0e0f26d606c616efa4
Dominik Moritz <domoritz@gmail.com>
2021-02-19 16:35:37 +0100

    add array cursor

--- src/table.rs
+++ src/table.rs
@@ -1,5 +1,6 @@
 use arrow::ipc;
 use arrow::{datatypes, error::ArrowError};
+use std::convert::TryFrom;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
@@ -54,6 +55,26 @@ impl Table {
         Table::from(&data.0)
     }
 
+    #[wasm_bindgen(js_name = fromUint8Array)]
+    pub fn from_array(array: js_sys::Uint8Array) -> Result<Table, JsValue> {
+        crate::utils::set_panic_hook();
+
+        let cursor = ArrayCursor::from(array);
+        let reader = match arrow::ipc::reader::FileReader::try_new(cursor) {
+            Ok(reader) => reader,
+            Err(error) => return Err(format!("{}", error).into()),
+        };
+
+        let schema = reader.schema();
+        match reader.collect() {
+            Ok(record_batches) => Ok(Table {
+                schema,
+                record_batches,
+            }),
+            Err(error) => Err(format!("{}", error).into()),
+        }
+    }
+
     pub fn serialize(&self) -> Result<js_sys::Uint8Array, JsValue> {
         let mut file = Vec::new();
         {
@@ -91,5 +112,45 @@ impl WasmUint8Array {
     #[wasm_bindgen(getter)]
     pub fn view(&mut self) -> js_sys::Uint8Array {
         unsafe { js_sys::Uint8Array::view_mut_raw(self.0.as_mut_ptr(), self.0.len()) }
+    }
+}
+
+pub struct ArrayCursor {
+    cursor: u32,
+    array: js_sys::Uint8Array,
+}
+
+impl ArrayCursor {
+    fn from(array: js_sys::Uint8Array) -> ArrayCursor {
+        ArrayCursor { cursor: 0, array }
+    }
+}
+
+impl std::io::Read for ArrayCursor {
+    fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
+        if self.cursor >= self.array.length() {
+            return Ok(0);
+        }
+
+        self.array.copy_to(buf);
+        buf[0] = self.array.get_index(self.cursor);
+        self.cursor += 1;
+        Ok(1)
+    }
+}
+
+impl std::io::Seek for ArrayCursor {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
+        match pos {
+            std::io::SeekFrom::Start(pos) => self.cursor = u32::try_from(pos).unwrap(),
+            std::io::SeekFrom::End(pos) => {
+                self.cursor = self.array.length() + u32::try_from(pos).unwrap()
+            }
+            std::io::SeekFrom::Current(pos) => {
+                self.cursor = self.cursor + u32::try_from(pos).unwrap()
+            }
+        };
+
+        Ok(u64::from(self.cursor))
     }
 }
